d1f2d62b4b6f4056f0b2af755cbe19a072cbb084
patrykzieba <patryk7465@gmail.com>
2020-01-28 17:10:33 +0100

    Changed

--- Projekt nr 3 note/script.js
+++ Projekt nr 3 note/script.js
@@ -8,32 +8,22 @@ function changeNoteColor(color){
 function add(){
     var NoteNumber=document.getElementById("Notecount").innerHTML;
     var NoteTittle = document.getElementById("titleinp").value;
-    var NoteImportant = document.getElementById("topImportantNote").firstChild.id;
     var NoteColor = document.getElementById("topNote").style.backgroundColor;
     var NoteText = document.getElementById("textinp").value;
     var NoteDate = new Date();
     const Note = {
         NoteNumber: NoteNumber,
         NoteTittle: NoteTittle,
-        NoteImportant: NoteImportant,
         NoteText: NoteText,
         NoteColor: NoteColor,
-        NoteDate: NoteDate.getDay()+" "+NoteDate.getMonth()+" "+NoteDate.getFullYear()
+        NoteDate: NoteDate
     }
     localStorage.setItem(NoteNumber, JSON.stringify(Note));
     countNote();
     const retrievedObject = localStorage.getItem(NoteNumber);
-    console.log(JSON.parse(retrievedObject).NoteImportant);
+    console.log(JSON.parse(retrievedObject));
     loadNotes();
     location.reload();
-}
-function important(val){
-    if(val.id=="true"){
-        document.getElementById("topImportantNote").innerHTML="<i class=\"far fa-star\" id=\"false\"  onclick=\"important(this)\"></i>";
-    }
-    else{
-        document.getElementById("topImportantNote").innerHTML="<i class=\"fas fa-star\" id=\"true\"  onclick=\"important(this)\"></i>";
-    }
 }
 function countNote(){
     var i=-1;
@@ -51,14 +41,6 @@ function loadNotes() {
     countNote();
     for (var NoteList = 0; NoteList < Number(document.getElementById("Notecount").innerHTML); NoteList++) {
         const retrievedObject = localStorage.getItem(NoteList);
-        let elemet = JSON.parse(retrievedObject);
-        var x = "";
-        if (JSON.parse(retrievedObject).NoteImportant == "true") {
-            x = "<i class=\"fas fa-star\"></i>";
-        }
-        else {
-            x = "<i class=\"far fa-star\"></i>";
-        }
         let note = document.createElement("div");
         note.className = "note";
         note.id = NoteList;
@@ -76,10 +58,7 @@ function loadNotes() {
         titleinp.style.width="90%";
         titleinp.style.marginLeft="10%";
 
-        let topImportantNote = document.createElement("div");
-        topImportantNote.id = "topImportantNote";
 
-        let importanto = document.createElement("p");
 
         let botNote = document.createElement("div");
         botNote.className = "botNote";
@@ -93,16 +72,12 @@ function loadNotes() {
         note.appendChild(topNote);
         note.appendChild(botNote);
         topNote.appendChild(topTitleNote);
-        topNote.appendChild(topImportantNote);
         botNote.appendChild(textinp);
         topTitleNote.appendChild(titleinp);
-        topImportantNote.appendChild(importanto);
         document.body.appendChild(note)
         topNote.style.backgroundColor = JSON.parse(retrievedObject).NoteColor;
         titleinp.innerHTML = JSON.parse(retrievedObject).NoteTittle;
         textinp.value = JSON.parse(retrievedObject).NoteText;
-        topImportantNote.innerHTML=x;
         note.style.float="left";
-        console.log(topNote.backgroundColor)
     }
 }
